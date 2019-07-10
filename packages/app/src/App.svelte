4395143a19eb1d75e3651bd4c04cae23f555a58a
Kiho <kihoc22@gmail.com>
2019-07-09 20:58:47 -0700

    Fix page layout

--- packages/app/src/App.svelte
+++ packages/app/src/App.svelte
@@ -4,14 +4,14 @@
   // const { preloading, page } = stores();
   import { fade } from "svelte/transition";
 
-  import { AppBar } from "smelte/src/components";
-  import { Tabs } from "smelte/src/components";
-  import { Button } from "smelte/src/components";
-  import { Spacer } from "smelte/src/components/Util";
-  import { List } from "smelte/src/components";
-  import { ListItem } from "smelte/src/components";
-  import { NavigationDrawer } from "smelte/src/components";
-  import { ProgressLinear } from "smelte/src/components";
+  import AppBar from "components/AppBar";
+  import Tabs from "components/Tabs";
+  import Button from "components/Button";
+  import { Spacer } from "components/Util";
+  import List from "components/List";
+  import ListItem from "components/List/ListItem.svelte";
+  import NavigationDrawer from "components/NavigationDrawer";
+  import ProgressLinear from "components/ProgressLinear";
 
   import {
     right,
@@ -24,48 +24,19 @@
   import AppRouter from './AppRouter.svelte';
   import { menu1, menu2, topMenu } from './menu';
 
-  let innerWidth = 0;
+  // const { page } = stores();
+
   let selected = "";
-
-  function calcBreakpoint(width) {
-    if (width > 1279) {
-      return "xl";
-    }
-    if (width > 1023) {
-      return "lg";
-    }
-    if (width > 767) {
-      return "md";
-    }
-    return "sm";
-  }
-
-  onMount(() => {
-    if (!process.browser) return;
-
-    breakpoint.update(() => calcBreakpoint(window.innerWidth));
-  });
-
-  function updateBreakpoint({ target }) {
-    const bp = calcBreakpoint(target.innerWidth);
-
-    return breakpoint.update(() => bp);
-  }
+  const bp = breakpoint();
+  // $: path = $page.path;
+  let path = '';
 
   const menu = menu1.concat(menu2).map(x =>({ to: x.to, text: x.text }));
-
-  function toggleNav() {
-    return showNavMobile.update(() => !$showNavMobile);
-  }
-
-  let path = '';
 </script>
 
 <svelte:head>
   <title>Smelte: Material design using Tailwind CSS for Svelte</title>
 </svelte:head>
-
-<svelte:window on:resize={updateBreakpoint} />
 
 <!-- {#if $preloading}
   <ProgressLinear app />
@@ -81,16 +52,20 @@
     <h6 class="pl-3 text-white tracking-widest font-thin text-lg">SMELTE</h6>
   </a>
   <Spacer />
-  <Tabs c="sm:hidden md:flex" items={topMenu} bind:selected={path} />
+  <Tabs navigation items={topMenu} bind:selected={path} />
   <div class="md:hidden">
-    <Button icon="menu" small text on:click={toggleNav} />
+    <Button
+      icon="menu"
+      small
+      text
+      on:click={() => showNavMobile.set(!$showNavMobile)} />
   </div>
   <a href="https://github.com/matyunya/smelte" class="px-4">
     <img src="/github.png" alt="Github Smelte" width="24" height="24" />
   </a>
 </AppBar>
 
-{#if $breakpoint}
+{#if $bp}
   <main
     class="container relative p-8 lg:max-w-3xl lg:ml-64 mx-auto mb-10 mt-24
     md:ml-56 md:max-w-md md:px-3"
@@ -101,7 +76,7 @@
       right={$right}
       persistent={$persistent}
       elevation={$elevation}
-      breakpoint={$breakpoint}>
+      breakpoint={$bp}>
       <h6 class="p-6 ml-1 pb-2 text-xs text-gray-900">Components</h6>
       <List items={menu}>
         <span slot="item" let:item class="cursor-pointer">
